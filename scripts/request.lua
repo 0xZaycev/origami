32dce2399ae44cd1425d5c3dd7b6b5a0a23b3b87
Alex Zaycev <alex@zaycev.dev>
2023-04-03 20:12:00 +0300

    Добавлено больше параметров в запросе, убрана логика распределения запросов

--- scripts/request.lua
+++ scripts/request.lua
@@ -3,12 +3,14 @@ local sender_node_id = ARGV[1]; -- инициатор запроса
 local request_id = ARGV[2];
 local channel = ARGV[3];
 local payload = ARGV[4];
+local group = ARGV[5];
+local no_response = ARGV[6];
+local timeout = ARGV[7];
 
 
 
 -- всякие ключи
 local base_path_key = "origami";
-local channels_base_path_key = base_path_key .. ":channels";
 local requests_base_path_key = base_path_key .. ":requests";
 
 local sender_key = base_path_key .. ":clients:" .. sender_node_id;
@@ -16,18 +18,9 @@ local sender_pending_pool_key = sender_key .. ":out:pending_pool";
 
 local pending_pool_base_path_key = requests_base_path_key .. ":pending_pool";
 local pending_pool_list_key = pending_pool_base_path_key .. ":list";
-local pending_pool_count_key = pending_pool_base_path_key .. ":count";
-
-local ack_pool_base_path_key = requests_base_path_key .. ":request_ack_pool";
-local ack_pool_list_key = ack_pool_base_path_key .. ":list";
-local ack_pool_count_key = ack_pool_base_path_key .. ":count";
 
 local request_base_path_key = requests_base_path_key .. ":" .. request_id;
 local request_key = request_base_path_key .. ":info";
-
-local channel_base_path_key = channels_base_path_key .. ":" .. channel;
-local channel_listeners_count_key = channel_base_path_key .. ":listeners_count";
-local channel_listeners_list_key = channel_base_path_key .. ":listeners_list";
 
 
 
@@ -37,7 +30,7 @@ local request_is_exists = redis.call("hincrby", request_key, "request_sender_ack
 if not (request_is_exists == 0) then
     -- если данный запрос уже кто-то добавляет, то не надо ему мешать, он сам все сделает
     -- так же, это может быть повторная отправка в случае разрыва соединения
-    redis.call('publish', "origami.c" .. sender_node_id, "1" .. request_id);
+    redis.call('publish', "origami.c" .. sender_node_id, request_id);
 
 
 
@@ -48,7 +41,7 @@ end;
 
 -- узнаем текущее время
 local time = redis.call("time");
-local timestamp = time[1] .. "." .. time[2];
+local timestamp = tonumber( time[1] .. "." .. time[2] );
 
 -- статусы:
 --      PENDING                 - запрос создан
@@ -65,11 +58,19 @@ redis.call('hmset', request_key,
     "state", "PENDING",
 
     "channel", channel,
+    "group_key", group,
+
+    "expired", timestamp + tonumber(timeout),
+    "timeout", timeout,
+    "try_after", (timestamp - 1),
+    "no_response", no_response,
+
     "request_id", request_id,
     "params", payload,
     "response", "",
-    "error", "",
+    "error", "0",
 
+    "request_expired", "-1",
     "request_sender_ack", "0",
     "request_executor_ack", "-1",
     "response_sender_ack", "-1",
@@ -90,107 +91,13 @@ redis.call('hmset', request_key,
 redis.call("hincrby", sender_key, "out_pending_requests", "1");
 redis.call("sadd", sender_pending_pool_key, request_id);
 
+-- добавляем в список ожидания
+redis.call("rpush", pending_pool_list_key, request_id);
+
 
 
 -- оповещаем иниатора что запрос был получен
-redis.call('publish', "origami.c" .. sender_node_id, "1" .. request_id);
-
-
--- пробуем сразу найти исполнитель для этого запроса
-local channel_listeners_count = redis.call("incrby", channel_listeners_count_key, "0");
-
-if channel_listeners_count == 0 then
-    -- нет исполнителей у канала, кладем запрос в стопку ожидания
-
-    redis.call("lpush", pending_pool_list_key, request_id);
-    redis.call("incr", pending_pool_count_key);
-
-    return 1;
-end;
-
--- если список исполнителей не пуст, то подбираем исполнителя
-local channel_listeners_list = redis.call('smembers', channel_listeners_list_key);
-
--- подбирать будем исполнителя с наименьшем кол-вом обрабатываемых в настоящий момент запросов
--- за эталон отсчета будем брать первого активого
-local listener_node_id = "";
-local listener_pending_requests = 0;
-
-
-
-for _, listener in pairs(channel_listeners_list) do
-    local listener_client_key = base_path_key .. ":clients:list:" .. listener;
-    local listener_active_pool_key = base_path_key .. ":clients:active_pool:" .. listener;
-
-
-
-    -- узнаем активен ли исполнитель
-    local listener_is_active = redis.call('exists', listener_active_pool_key);
-
-    if listener_is_active == 1 then
-        -- исполнитель активен, заполняем переменные
-
-        local _listener_pending_requests = tonumber( redis.call("hget", listener_client_key, "in_pending_requests") );
-
-        if listener_node_id == "" then
-            -- это первый исполнитель — просто заполняем переменные
-
-            listener_node_id = listener;
-            listener_pending_requests = _listener_pending_requests;
-        else
-            -- проверим кол-во исполняемых запросов, если меньше, то перепишем переменные
-
-            if _listener_pending_requests < listener_pending_requests then
-                listener_node_id = listener;
-                listener_pending_requests = _listener_pending_requests;
-            end;
-        end;
-    end;
-end;
-
-
-
--- проверим (на всякий случай) если ли исполнитель
-if listener_node_id == "" then
-    -- исполнителя нет — кладем запрос в стопку ожидания
-
-    redis.call("lpush", pending_pool_list_key, request_id);
-    redis.call("incr", pending_pool_count_key);
-
-    return 1;
-end;
-
-
-
--- кладем запрос в стопку ожидания подтверждения исполнителя
-redis.call('lpush', ack_pool_list_key, request_id);
-redis.call("incr", ack_pool_count_key);
-
-
-
--- переменные для работы с клиентом
-local listener_key = base_path_key .. ":clients:" .. listener_node_id;
-local listener_pending_pool_key = listener_key .. ":in:pending_pool";
-
--- так же кладем запрос к исполнителю
-redis.call("hincrby", listener_key, "in_pending_requests", "1");
-redis.call("sadd", listener_pending_pool_key, request_id);
-
-
-
--- устанавляваем исполнителя и меняем статус у запроса
-redis.call("hmset", request_key,
-    "executor_node_id", listener,
-
-    "state", "WAIT_REQUEST_ACK",
-
-    "sent_to_executor_at", timestamp
-);
-
-
-
--- высылаем ивент исполнителю
-redis.call("publish", "origami.b" .. listener .. channel, request_id .. payload);
+redis.call('publish', "origami.c" .. sender_node_id, request_id);
 
 
 
