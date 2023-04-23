f1b34285107225727686bdd2f4f6b7a7a8493fd7
Alex Zaycev <alex@zaycev.dev>
2023-04-23 04:15:58 +0300

    Добавлен индексный файл для экспорта необходимого функционала

---
+++ packages/js/src/index.ts
@@ -0,0 +1,6 @@
+export {Origami, IOrigamiOptions, EOrigamiState} from './Origami';
+
+export {IChannelOptions, IChannelHandler} from './Consumer';
+export {IRequestOptions, TRequestResponse, EResponseCode} from './Producer';
+
+export {TBaseResult, BaseResult} from './utils/BaseResult';
